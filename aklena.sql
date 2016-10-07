bf96f2dbe2389f768d3a9882665e01e88e037186
Moustafa Mowaffak <mmowaffak@gmail.com>
2016-10-08 01:10:55 +0200

    Adding database schema

---
+++ aklena.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 06, 2016 at 04:23 PM
+-- Server version: 10.1.9-MariaDB
+-- PHP Version: 7.0.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `aklena`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `fname` varchar(15) DEFAULT NULL,
+  `lname` varchar(15) DEFAULT NULL,
+  `email` varchar(25) DEFAULT NULL,
+  `uid` int(11) NOT NULL,
+  `eventumExt` varchar(3) DEFAULT NULL,
+  `phone` varchar(13) DEFAULT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`fname`, `lname`, `email`, `uid`, `eventumExt`, `phone`, `password`) VALUES
+('Yasmin', 'Tayel', 'ytayel@eventumsolutions.c', 1, '125', '01223211344', ''),
+('Ahmed', 'Kabbany', 'akabbany@eventumsolutions', 2, '126', '0122336532', ''),
+('Moustafa', 'Mowaffak', 'mmowaffak@eventumsolution', 3, '125', '01287897960', '123456');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`uid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `uid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
